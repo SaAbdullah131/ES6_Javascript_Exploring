0de65f5d857d9845caee0b386a2db80eea87257d
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-02-23 12:18:24 +0600

    templating in js

---
+++ template.js
@@ -0,0 +1,24 @@
+const person = 'Adan Sandler';
+const person2 = "Ben White";
+const person3 = `Donala Trmup`;
+
+const multiLine = 'Fine Line Text\n' + 
+'second Line of code \n' + 
+'Third Line of Text\n' + 
+'Fourth Line of String';
+// console.log(multiLine);
+
+const newMultiLine= `First Line of Text
+Second Line of Text
+Third Line of String 
+Fourth Line of code`;
+
+// console.log(newMultiLine);
+
+const a = 20 ;
+const b = 30;
+const nums = [78,98,45,12,6,444];
+const summary = 'sum of ' + a + 'and' + b + ' is' + (a+b);
+// console.log(summary);
+const newSummary = `Sum of ${a} and ${b} is : ${a+b} and length of nums Array  is: ${nums.length}`;
+console.log(newSummary);
